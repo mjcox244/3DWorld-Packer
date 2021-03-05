1d0036db6e31b71db5ba2001d45f7522222bd507
mjcox244 <66311016+mjcox244@users.noreply.github.com>
2021-03-05 11:03:11 +0000

    Added notation to the program

--- 3D world packer CS/3DWPacker/Program.cs
+++ 3D world packer CS/3DWPacker/Program.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 
 namespace _3DWPacker {
     class Program {
         static void Main(string[] args) {
+
+            //takes in input from thr comand prompt
             Console.WriteLine("What is your mod name?");
             string modName = Console.ReadLine();
             Console.WriteLine("Enter a short 1 line description of your mod...");
@@ -12,11 +14,14 @@ namespace _3DWPacker {
             Console.WriteLine("Drag and drop the contents folder here!");
             string modDir = Console.ReadLine().Replace("\"", "");
 
+            //makes a contents directory 
             Directory.CreateDirectory(Path.Combine(modName, "contents"));
 
+            //Copys the contents folder the user dragged in earlyer to the output directory
             Console.WriteLine("\nPacking the mod...");
             RecursiveCopy(new DirectoryInfo(modDir), new DirectoryInfo(Path.Combine(modName, "contents")));
 
+            //this makes the rules.txt for cemu to use
             Console.WriteLine("Generating \"rules.txt\"...");
             File.WriteAllText(Path.Combine(modName, "rules.txt"),
                 "[Definition]\n" +
@@ -26,9 +31,11 @@ namespace _3DWPacker {
                 "\ndescription = " + modDesc +
                 " \nversion = 5");
 
+            //simple press enter to exit prompt
             Console.Write("\nSuccess! Press any key to exit...");
             Console.ReadKey();
 
+            //this opens the output directory
             Process exp = new Process();
             exp.StartInfo.FileName = "explorer.exe";
             exp.StartInfo.Arguments = Path.Combine(Directory.GetCurrentDirectory(), modName);
@@ -37,6 +44,7 @@ namespace _3DWPacker {
             exp.Start();
         }
 
+        //copy Directorys and sub directorys
         static void RecursiveCopy(DirectoryInfo origin, DirectoryInfo target) {
             foreach(DirectoryInfo dir in origin.GetDirectories())
                 RecursiveCopy(dir, target.CreateSubdirectory(dir.Name));
